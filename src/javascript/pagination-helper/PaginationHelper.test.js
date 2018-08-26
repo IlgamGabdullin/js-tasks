8832acd2d29cc39b89922a17ce41f890baabe5fe
Ilgam <ilgamgabdullin@yandex.ru>
2018-08-26 11:26:45 +0300

    writed tests for PaginationHelper class

--- src/javascript/pagination-helper/PaginationHelper.test.js
+++ src/javascript/pagination-helper/PaginationHelper.test.js
@@ -2,9 +2,48 @@ import assert from 'assert';
 import { PaginationHelper } from './PaginationHelper';
 
 describe('PaginationHelper', () => {
-  const helper = new PaginationHelper(['a', 'b', 'c', 'd'], 2);
+  const itemsPerPage = 4;
+  const helper = new PaginationHelper(['a','b','c','d','e','f'], itemsPerPage);
 
-  it('Method "itemCount"  should return 2', () => {
+  it("Method 'itemCount' should return 2", () => {
     assert.equal(helper.itemCount(), 2);
+  });
+
+  it("Method 'pageCount' should return 2", () => {
+    assert.equal(helper.pageCount(), 6);
+  });
+
+  describe('PaginationHelper.pageItemCount() method', () => {
+    it("pageItemCount(0) should return 4", () => {
+      const items = helper.pageItemCount(0);
+      assert.equal(items, 4);
+    });
+
+    it("pageItemCount(1) should return 2 (as last page)", () => {
+      const items = helper.pageItemCount(1);
+      assert.equal(items, 2);
+    });
+
+    it("pageIemCount(2) should return -1 (page is invalid)", () => {
+      const items = helper.pageItemCount(2);
+      assert.equal(items, -1);
+    });
+  });
+
+  describe("PaginationHelper.pageIndex() method", () => {
+    it("pageIndex(5) should return 1 (zero based index)", () => {
+      const page = helper.pageIndex(5);
+      assert.equal(page, 1);
+    });
+
+    it("pageIndex(2) should return 0", () => {
+      const page = helper.pageIndex(2);
+      assert.equal(page, 0);
+    });
+
+    it("pageIndex(20) should return -1", () => {
+      const page = helper.pageIndex(20);
+      assert.equal(page, -1);
+    });
   });
 });
