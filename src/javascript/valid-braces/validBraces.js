a76f04ac08f01fea07b5c8dce12d8bb170096ec9
Ilgam <ilgamgabdullin@yandex.ru>
2018-08-26 22:18:00 +0300

    implemented validBraces function

--- src/javascript/valid-braces/validBraces.js
+++ src/javascript/valid-braces/validBraces.js
@@ -7,5 +7,19 @@ const bracesMap = {
 export const validBraces = (string) => {
   const braces = [];
 
-  return false;
+  for (let i = 0; i < string.length; i++) {
+    if(!bracesMap.hasOwnProperty(string[i])) {
+      braces.push(string[i]);
+    } else {
+      const lastBrace = braces[braces.length - 1];
+
+      if (bracesMap[string[i]] === lastBrace) {
+        braces.pop()
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return !braces.length;
 }
